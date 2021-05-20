67c1f830eb9d7e1d45ee342c2f471b3415a5e235
vsk <vsk@LAPTOP-2P7KRBE3>
2021-05-20 17:46:01 +0530

    getters and setters

--- LibraryManagementSystem/src/main/java/com/capgemini/lms/entities/Author.java
+++ LibraryManagementSystem/src/main/java/com/capgemini/lms/entities/Author.java
@@ -9,4 +9,43 @@ public class Author {
 	private String email;
 	private String contactno;
 	private List<Books> books;
+	
+	public int getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactno() {
+		return contactno;
+	}
+	public void setContactno(String contactno) {
+		this.contactno = contactno;
+	}
+	public List<Books> getBooks() {
+		return books;
+	}
+	public void setBooks(List<Books> books) {
+		this.books = books;
+	}
+	
+	
 }
