2d2c448a04c1e5e4618f45c7a98de371cd28a654
Neelima <51400319+NeelimaDurgam@users.noreply.github.com>
2021-05-21 09:59:08 +0530

    Update Author.java

--- LibraryManagementSystem/src/main/java/com/capgemini/lms/entities/Author.java
+++ LibraryManagementSystem/src/main/java/com/capgemini/lms/entities/Author.java
@@ -1,15 +1,44 @@
 package com.capgemini.lms.entities;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import java.util.List;
 
+
+@Entity
+@Table(name = "author")
 public class Author {
+	@Id
+	@Column(name = "author_id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int authorId;
+	@Column(name = "first_name")
 	private String firstName;
+	@Column(name = "last_name")
 	private String lastName;
+	@Column(name = "email")
 	private String email;
+	@Column(name = "contact_no")
 	private String contactno;
+	@Column(name = "books")
 	private List<Books> books;
 	
+	
+	public Author(int authorId, String firstName, String lastName, String email, String contactno, List<Books> books) {
+		super();
+		this.authorId = authorId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.contactno = contactno;
+		this.books = books;
+	}
 	public int getAuthorId() {
 		return authorId;
 	}
@@ -46,6 +75,5 @@ public class Author {
 	public void setBooks(List<Books> books) {
 		this.books = books;
 	}
-	
 	
 }
